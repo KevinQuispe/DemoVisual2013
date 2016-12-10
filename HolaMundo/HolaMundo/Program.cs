6982e632355b24a75351563ccd248ebc142efb7d
QUISPE <kquispe07@hotmail.com>
2016-12-09 23:10:35 -0500

    primera version

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //esta es mi primera version para mi programa en c#
+            Console.WriteLine("Hola Mundo de Programadores");
+            Console.ReadLine();
+        }
+    }
+}
